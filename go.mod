cb4e80e49c7fe6c80ff469e32b02073806a545d2
Marco Munizaga <git@marcopolo.io>
2021-01-05 14:41:46 -0800

    Add ping

--- go.mod
+++ go.mod
@@ -3,11 +3,16 @@ module github.com/marcopolo/fast-honeycomb-reporter
 go 1.13
 
 require (
+	github.com/antonholmquist/jason v1.0.0 // indirect
 	github.com/ddo/go-fast v0.0.0-20190807090025-2ab4a3dd618c
+	github.com/ddo/go-spin v0.0.0-20160718105357-ab0a40e623c0 // indirect
 	github.com/ddo/pick-json v0.0.0-20170207095303-c8760e09e0fe // indirect
 	github.com/ddo/rq v0.0.0-20190828174524-b3daa55fcaba // indirect
+	github.com/go-ping/ping v0.0.0-20201115131931-3300c582a663
 	github.com/honeycombio/libhoney-go v1.12.4
 	github.com/vmihailenco/msgpack/v4 v4.3.12 // indirect
+	gopkg.in/ddo/go-dlog.v1 v1.0.2 // indirect
 	gopkg.in/ddo/go-dlog.v2 v2.1.0 // indirect
 	gopkg.in/ddo/pick.v1 v1.2.2 // indirect
+	gopkg.in/ddo/request.v1 v1.4.2 // indirect
 )
--- main.go
+++ main.go
@@ -2,18 +2,36 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/ddo/go-fast"
+	"github.com/go-ping/ping"
 	"github.com/honeycombio/libhoney-go"
 )
+
+const waitBetweenDownloads = 30 * time.Minute
+const waitVariance = 1 * time.Minute
 
 func main() {
 	libhoney.Init(libhoney.Config{
 		WriteKey: os.Getenv("HONEYCOMB_API_KEY"),
 		Dataset:  "Mammoth Lakes Internet",
 	})
+	defer libhoney.Close()
 
+	go pingTest()
+
+	for {
+		downloadSpeedTest()
+		secs := time.Duration(waitVariance.Seconds()*rand.Float64()) * time.Second
+		time.Sleep(waitBetweenDownloads + secs)
+	}
+}
+
+func downloadSpeedTest() {
 	fastCom := fast.New()
 
 	// init
@@ -42,12 +60,53 @@ func main() {
 			fmt.Printf("%.2f Kbps %.2f Mbps\n", Kbps, Kbps/1000)
 		}
 
-		fmt.Println("done")
-		libhoney.Close()
 	}()
 
 	err = fastCom.Measure(urls, KbpsChan)
 	if err != nil {
 		panic(err)
 	}
+
+}
+
+func pingTest() {
+	pinger, err := ping.NewPinger("1.1.1.1")
+	if err != nil {
+		panic(err)
+	}
+
+	// Listen for Ctrl-C.
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for _ = range c {
+			pinger.Stop()
+		}
+	}()
+
+	pinger.OnRecv = func(pkt *ping.Packet) {
+		event := libhoney.NewEvent()
+		event.Add(map[string]interface{}{
+			"latency_ms": pkt.Rtt.Milliseconds,
+		})
+		event.Send()
+
+		fmt.Printf("%d bytes from %s: icmp_seq=%d time=%v\n",
+			pkt.Nbytes, pkt.IPAddr, pkt.Seq, pkt.Rtt)
+	}
+
+	pinger.OnFinish = func(stats *ping.Statistics) {
+		fmt.Printf("\n--- %s ping statistics ---\n", stats.Addr)
+		fmt.Printf("%d packets transmitted, %d packets received, %v%% packet loss\n",
+			stats.PacketsSent, stats.PacketsRecv, stats.PacketLoss)
+		fmt.Printf("round-trip min/avg/max/stddev = %v/%v/%v/%v\n",
+			stats.MinRtt, stats.AvgRtt, stats.MaxRtt, stats.StdDevRtt)
+	}
+
+	fmt.Printf("PING %s (%s):\n", pinger.Addr(), pinger.IPAddr())
+	err = pinger.Run()
+	if err != nil {
+		panic(err)
+	}
+
 }
