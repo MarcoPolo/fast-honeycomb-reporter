03269df81645d01318d8e400ee4b70b115c6ad4f
Marco Munizaga <git@marcopolo.io>
2021-01-05 17:13:29 -0800

    Add nixos module

--- flake.nix
+++ flake.nix
@@ -7,7 +7,31 @@
       (system:
         let pkgs = nixpkgs.legacyPackages.${system}; in
         {
-          defaultPackage = import ./fast-honeycomb-reporter.nix {inherit pkgs;};
+          defaultPackage = import ./fast-honeycomb-reporter.nix { inherit pkgs; };
+          nixosModule = { config, pkgs, ... }:
+            let cfg = config.services.fast-honeycomb-reporter;
+            in
+            {
+              options.services.fast-honeycomb-reporter = {
+                apiKey = nixpkgs.lib.mkOption {
+                  description = "Honeycomb API Key";
+                  type = nixpkgs.lib.types.str;
+                };
+              };
+              config = {
+                users.users.honeycomb-reporter.isSystemUser = true;
+                systemd.services.honeycomb-reporter = {
+                  description = "fast-honeycomb-reporter";
+                  wantedBy = [ "multi-user.target" ];
+                  after = [ "network.target" ];
+                  serviceConfig = {
+                    ExecStart = "HONEYCOMB_API_KEY=${cfg.apiKey} ${self.defaultPackage.${system}}/bin/fast-honeycomb-reporter";
+                    Restart = "on-failure";
+                    User = "honeycomb-reporter";
+                  };
+                };
+              };
+            };
         }
       );
 }
